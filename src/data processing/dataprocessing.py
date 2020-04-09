faa2b0a697506d8a0daac4483fe1a8c993baad0a
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2020-04-08 22:39:46 -0700

    Delete dataprocessing.py

--- src/data processing/dataprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-dataframe = pd.read_csv(url)
-dataframe = dataframe.drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1)
-
-global_numbers = dataframe.sum(axis = 0, skipna=True)
-global_numbers.to_csv('global_data.csv')
